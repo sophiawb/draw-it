8c482f3e198e8c2137bf71284e1c31b174fec8fc
Hack Reactor Students <accounts@hackreactor.com>
2015-07-11 12:30:09 -0700

    Visualize data

--- drawIt.js
+++ drawIt.js
@@ -1,41 +1,21 @@
-// Update title text in .title
+d3.select('.title').text(title);
 
 
-// Get data from file
-
-var minX = 1960;// get value from data, or set manually
+var minX = 1960;
 var maxX = 2015;
 var minY = 60;
 var maxY = 80;
 
+data = data.data.map(function(d) { return {x: +d[0].slice(0,4), y: d[1]}; });
 
 var svg = d3.select(".graph");
-/*
 
-var makeScale = (direction, domain, range) {
-  var scale = d3.scale.linear()
-    .domain(domain)
-    .range(range);
 
-  var axis = d3.svg.axis()
-    .scale(scale)
-    .orient(direction)
-    .ticks(10); // "best guess"
 
-  var g = svg.append("g");
-  axis(g);
-  g.attr("transform", "translate(50,100)");
-  g.selectAll("path")
-    .style({ fill: "none", stroke: "#000"});
-  g.selectAll("line")
-    .style({ stroke: "#000"});
 
-}
 
-makeScale("bottom", [minX, maxX], [100,700]);
-//makeScale("left", [minY, maxY], [100,100]);
-*/
 
+// Set up axes
 
 var xScale = d3.scale.linear()
   .domain([minX, maxX])
@@ -78,4 +58,20 @@ yg.selectAll("path")
 yg.selectAll("line")
   .style({ stroke: "#000"});
 
+
+// Draw the graph
+
+// var x = d3.scale.ordinal()
+//   .domain(d3.extent(data, function(d) {return d.x; }));
+
+// var y = d3.scale.ordinal()
+//   .domain(d3.extent(data, function(d) {return d.y; }));
+
+var valueline = d3.svg.line()
+  .x(function(d) { return xScale(d.x); })
+  .y(function(d) { return yScale(d.y); });
+
+svg.append("path")
+  .attr("class", "line")
+  .attr("d", valueline(data));
 
